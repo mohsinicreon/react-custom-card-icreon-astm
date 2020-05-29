cdbd68ba535756f0faf27b2ca0cb63d2de8cb821
mohsin02 <mohsin.ali@icreon.com>
2020-05-29 19:41:08 +0530

    Code Refactor

---
+++ build/precache-manifest.f58d6d4efaf8693e0352435316348b42.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01cbbc3a22990c09dc602edc7f317f10",
+    "url": "/index.html"
+  },
+  {
+    "revision": "32b0d6bf8110d68c105e",
+    "url": "/static/css/main.c4c147ca.chunk.css"
+  },
+  {
+    "revision": "ead70de674b354e3b22c",
+    "url": "/static/js/2.2f5b302d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2f5b302d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32b0d6bf8110d68c105e",
+    "url": "/static/js/main.c45065b5.chunk.js"
+  },
+  {
+    "revision": "acb6204410ea228609e9",
+    "url": "/static/js/runtime-main.c02386aa.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
