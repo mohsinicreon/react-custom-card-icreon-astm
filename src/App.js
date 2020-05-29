302c10777845ce13aeafae08d9fd4171f9865f74
mohsin02 <mohsin.ali@icreon.com>
2020-05-29 14:40:07 +0530

    creating a repo

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import Card from './components/Card';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Testing for publishing NPM with create-react-app
+        </a>
+
+        <Card>
+          <h1>Hello Developer!, <br />I am new card component!</h1>
+        </Card>
+      </header>
+    </div>
+  );
+}
+export default App;
